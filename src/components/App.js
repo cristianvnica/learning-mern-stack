4e4870dfbcf52c6a08f202bdee60d5414edb5d09
cristiavnica <cristian.nica@live.com>
2017-08-24 17:41:38 +0300

    Updating changes

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import axios from 'axios';
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests',
+    contests: []
+  };
+  componentDidMount() {
+    axios.get('/api/contests')
+      .then(resp => {
+        this.setState({
+          contests: resp.data.contests
+        });
+      })
+      .catch(console.error)
+  }
+  componentWillUnmount() {
+  }
+  render() {
+    return(
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          {this.state.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest}/>
+          )}
+        </div>
+      </div>
+    );
+  }
+};
+
+export default App;
